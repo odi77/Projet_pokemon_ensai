9f86497479e0a056d6acef017c5a3432815c1a80
@cnebanga <cnebanga@gmail.com>
2022-10-11 21:54:38 +0200

    classe Attacker est codée

--- business_object/pokemon/attacker.py
+++ business_object/pokemon/attacker.py
@@ -6,11 +6,14 @@ class Attacker(AbstractPokemon):
 
     def __init__(self, current_stat, level, name, speed_current, speed_attack):
         super.__init__(self, current_stat, level, name)
+        self.speed_current= speed_current
+        self.speed_attack = speed_attack
 
 
 
     def get_pokemon_attack_coef(self):
-        return
+        return 1 + (self.speed_current + self.speed_attack)/200
+
 
 
 
