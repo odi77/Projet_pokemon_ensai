38f4f853542da89be8f131724fc7208b06b80268
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-10-12 18:34:57 +0200

    main

--- business_object/pokemon/abstract_pokemon.py
+++ business_object/pokemon/abstract_pokemon.py
@@ -1,7 +1,7 @@
 """classe abstraite pokemon """
 
 from abc import ABC, abstractmethod
-from Projet_pokemon_ensai.business_object.statistique import Statistique
+from business_object.statistique import Statistique
 
 class AbstractPokemon(ABC):
     """_summary_
